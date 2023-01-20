56bc4ecb116103e23209d7f1bb1419c450fef03d
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-01-20 09:02:42 +0900

    chore: createUser async/await으로 변경

--- functions/createUser.js
+++ functions/createUser.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
   const { position, department, name, email, id } = req.body.userInfo;
 
   const emailValid = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
