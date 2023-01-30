0b31fe58335471b8a9f2a5ae55184595a9a52707
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-01-30 18:36:28 +0900

    chore: 달력 스타일링

--- screens/AnnualLeaveScreen.jsx
+++ screens/AnnualLeaveScreen.jsx
@@ -39,8 +39,8 @@ LocaleConfig.locales['ko'] = {
     '11월',
     '12월'
   ],
-  dayNames: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
-  dayNamesShort: ['월', '화', '수', '목', '금', '토', '일.'],
+  dayNames: ['일요일', '화요일', '수요일', '목요일', '금요일', '토요일', '월요일'],
+  dayNamesShort: ['일', '화', '수', '목', '금', '토', '월'],
   today: "오늘"
 };
 LocaleConfig.defaultLocale = 'ko';
@@ -60,7 +60,7 @@ LocaleConfig.defaultLocale = 'ko';
  * 연차에 맞게끔 초기화 후 알맞는 값을 넣어줌.
  */
 const now = moment();
-
+const formattedToday =  moment().format('yyyy-MM-DD')
 
 const AnnualLeaveScreen = () => {
   const [date, setDate] = useState(new Date());
@@ -68,7 +68,7 @@ const AnnualLeaveScreen = () => {
   const [diffMonth,setDiffMonth] = useState();
   const [diffYear,setDiffYear] = useState();
   const [isShowCalander,setIsShowCalander] = useState(false);
-  const [isShowAppointVacationModal,setIsShowAppointVacationModal] = useState(false);
+  const [isShowAppointVacationModal,setIsShowAppointVacationModal] = useState(true);
   const { postAnnualLeave,getAnnualLeave,annual } = useAnnualLeave();
   const userInfo = useRecoilValue(userInfoState)
   const navigation = useNavigation();
@@ -110,7 +110,7 @@ const AnnualLeaveScreen = () => {
     console.log('excuted');
     setIsShowAppointVacationModal(true);
   }
-
+  
   useLayoutEffect(() => {
     getAnnualLeave(userInfo.email);
   }, [])
@@ -137,61 +137,76 @@ const AnnualLeaveScreen = () => {
     >
     
       <Calendar
-      style={{flex:1,backgroundColor:'black'}}
-      
-      // Initially visible month. Default = now
-      initialDate={'2012-03-01'}
-      // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-      minDate={'2012-05-10'}
-      // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-      maxDate={'2012-05-30'}
-      // Handler which gets executed on day press. Default = undefined
+      // // Initially visible month. Default = now
+      initialDate={formattedToday}
+      // minDate={'2012-05-10'}
+      // maxDate={'2012-05-30'}
+      // // Handler which gets executed on day press. Default = undefined
       onDayPress={day => {
         console.log('selected day', day);
       }}
-      // Handler which gets executed on day long press. Default = undefined
-      onDayLongPress={day => {
-        console.log('selected day', day);
+      theme={{
+        monthTextColor:'#2d63e2',
+        textMonthFontSize:24,
+        arrowWidth:18,
+        selectedDayTextColor:'#f41',
+        selectedDotColor:'#f41',
+        'stylesheet.calendar.header': {
+          dayTextAtIndex0: {
+            color:'#f41'
+          },
+          dayHeader:{
+            color:'#000'
+          },
+          dayTextAtIndex6: {
+            color:'#2d63e2'
+          }
+        }
       }}
+      // // Handler which gets executed on day long press. Default = undefined
+      // onDayLongPress={day => {
+      //   console.log('selected day', day);
+      // }}
       // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-      monthFormat={'yyyy MM'}
-      // Handler which gets executed when visible month changes in calendar. Default = undefined
-      onMonthChange={month => {
-        console.log('month changed', month);
-      }}
-      // Hide month navigation arrows. Default = false
-      hideArrows={true}
-      // Replace default arrows with custom ones (direction can be 'left' or 'right')
-      renderArrow={direction => <Arrow />}
-      // Do not show days of other months in month page. Default = false
-      hideExtraDays={true}
-      // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
-      // day from another month that is visible in calendar page. Default = false
-      disableMonthChange={true}
-      // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
-      firstDay={1}
-      // Hide day names. Default = false
-      hideDayNames={true}
-      // Show week numbers to the left. Default = false
-      showWeekNumbers={true}
-      // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-      onPressArrowLeft={subtractMonth => subtractMonth()}
-      // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-      onPressArrowRight={addMonth => addMonth()}
-      // Disable left arrow. Default = false
-      disableArrowLeft={true}
-      // Disable right arrow. Default = false
-      disableArrowRight={true}
-      // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-      disableAllTouchEventsForDisabledDays={true}
-      // Replace default month and year title with custom one. the function receive a date as parameter
-      renderHeader={date => {
-        /*Return JSX*/<View>
-          {date}
-        </View>
-      }}
-      // Enable the option to swipe between months. Default = false
-      enableSwipeMonths={true}
+      monthFormat={'yyyy년 MM월'}
+      
+      // // Handler which gets executed when visible month changes in calendar. Default = undefined
+      // onMonthChange={month => {
+      //   console.log('month changed', month);
+      // }}
+      // // Hide month navigation arrows. Default = false
+      // hideArrows={true}
+      // // Replace default arrows with custom ones (direction can be 'left' or 'right')
+      // renderArrow={direction => <Arrow />}
+      // // Do not show days of other months in month page. Default = false
+      // hideExtraDays={true}
+      // // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+      // // day from another month that is visible in calendar page. Default = false
+      // disableMonthChange={true}
+      // // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+      // firstDay={1}
+      // // Hide day names. Default = false
+      // hideDayNames={true}
+      // // Show week numbers to the left. Default = false
+      // showWeekNumbers={true}
+      // // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+      // onPressArrowLeft={subtractMonth => subtractMonth()}
+      // // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+      // onPressArrowRight={addMonth => addMonth()}
+      // // Disable left arrow. Default = false
+      // disableArrowLeft={true}
+      // // Disable right arrow. Default = false
+      // disableArrowRight={true}
+      // // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+      // disableAllTouchEventsForDisabledDays={true}
+      // // Replace default month and year title with custom one. the function receive a date as parameter
+      // renderHeader={date => {
+      //   /*Return JSX*/<View>
+      //     {date}
+      //   </View>
+      // }}
+      // // Enable the option to swipe between months. Default = false
+      // enableSwipeMonths={true}
     />
     
     </Modal>
