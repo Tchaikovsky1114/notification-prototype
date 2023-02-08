3194de75a0c4042781f448a00cba4916ca165509
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-02-08 14:29:31 +0900

    refactoring: 실패시 전부 취소 될 수 있게 batch 묶음

--- functions/endWork.js
+++ functions/endWork.js
@@ -4,6 +4,8 @@ const admin = require('firebase-admin');
 module.exports = async function(req,res) {
   const { leave,date,email } = req.body;
 
+  const batch = admin.firestore().batch();
+
   const attendanceRef = admin.firestore().collection('Attendance').doc(`${email}`);
   
   const attendanceData = await attendanceRef.get();
@@ -12,18 +14,28 @@ module.exports = async function(req,res) {
 
   const removeTarget = attendanceRecord.find((item) => item.date === date);
 
-  await attendanceRef.update({
-    attendance: admin.firestore.FieldValue.arrayRemove(removeTarget),
-  })
+  // await attendanceRef.update({
+  //   attendance: admin.firestore.FieldValue.arrayRemove(removeTarget),
+  // })
+  batch.update(
+    attendanceRef,{ attendance: admin.firestore.FieldValue.arrayRemove(removeTarget) }
+  )
 
   const obj = {
     ...removeTarget,
     "leave": leave,
   }
+  
+  // await attendanceRef.set({
+  //   attendance: admin.firestore.FieldValue.arrayUnion(obj),
+  // },{ merge: true })
+  batch.set(
+    attendanceRef, { attendance: admin.firestore.FieldValue.arrayUnion(obj) },{ merge: true }
+  )
 
-  await attendanceRef.set({
-    attendance: admin.firestore.FieldValue.arrayUnion(obj),
-  },{ merge: true })
+
+  await batch.commit();
+
   const newAttendance = await attendanceRef.get();
   const newAttendanceRecord = newAttendance.data().attendance;
   return await res.json({message: '퇴근시간이 저장되었습니다.',attendance:newAttendanceRecord});
